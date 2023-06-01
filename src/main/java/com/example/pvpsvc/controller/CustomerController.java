dd962f143b72d7cfe10786024aed96fca992af5b
Andiyappan, Prabhakaran (FIS) <prabhakaran.andiyappan@troweprice.com>
2023-06-01 16:33:13 +0530

    [initial-commit]

---
+++ src/main/java/com/example/pvpsvc/controller/CustomerController.java
@@ -0,0 +1,30 @@
+package com.example.pvpsvc.controller;
+
+import com.example.pvpsvc.model.entity.CustomerEntity;
+import com.example.pvpsvc.model.request.CustomerRequest;
+import com.example.pvpsvc.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping(path="/api/customer/query",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<List<CustomerEntity>> getCustomerAns( @RequestBody CustomerRequest request) {
+        List<CustomerEntity> list = customerService.getCustomerAns(request);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+}
